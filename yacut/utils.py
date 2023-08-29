fc0efb606130fc682e6661c814f9898773c6bd57
Iskander <logick7@mail.ru>
2023-08-30 02:30:21 +0300

    review4

--- yacut/utils.py
+++ yacut/utils.py
@@ -4,6 +4,8 @@ from .models import URLMap
 
 
 def get_short(short):
+    if short:
+        return short
     symbols = string.ascii_letters + string.digits
     while True:
         new_short = ''.join((random.choice(symbols) for _ in range(6)))
